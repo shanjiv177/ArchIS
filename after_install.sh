d10d82612a9673546d849825e3052dc1e9b0be34
Shanjiv A <65117343+shanjiv177@users.noreply.github.com>
2021-11-09 19:04:51 +0530

    Add files via upload

---
+++ after_install.sh
@@ -0,0 +1,29 @@
+cd
+
+sudo pacman -S --no-confirm xorg gnome gnome-tweaks gparted zsh zsh-autosuggestions zsh-syntax-highlighting firefox noto-fonts ttf-croscore ttf-dejavu ttf-droid ttf-ubuntu-font-family ttf-indic-otf adobe-source-code-pro-fonts ttf-roboto gnu-free-fonts ttf-liberation
+
+sudo systemctl enable gdm
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd 
+
+echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> .zshrc
+echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> .zshrc
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si PKGBUILD
+
+cd
+
+yay -S --no-confirm ttf-ms-fonts zoom
+
+yay -S --no-confirm flatpak snapd snapd-glib
+
+sudo pacman -S --no-confirm libreoffice-fresh vlc audacity kdenlive telegram-desktop
+
+sudo reboot
+
