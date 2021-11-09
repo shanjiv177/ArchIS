1189602ba0de1c321ab7f7c8c2b31055820ff55c
Shanjiv A <65117343+shanjiv177@users.noreply.github.com>
2021-11-09 17:42:06 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,17 +1,18 @@
 timedatectl set-ntp true
 
-mkfs.ext4 /dev/vda2
-mkswap /dev/vda1
+mkfs.ext4 /dev/sda2
+mkswap /dev/sda1
 
-mount /dev/vda2 /mnt
-swapon /dev/vda1
+mount /dev/sda2 /mnt
+swapon /dev/sda1
 
 pacman -S --no-confirm reflector
 
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
 reflector -l 10 -a 6 --sort rate --save /etc/pacman.d/mirrorlist --verbose
- pacstrap -S linux linux-firmware nano vim intel-ucode linux-headers linux-docs broadcom-wl
+
+pacstrap -S linux linux-firmware nano vim intel-ucode linux-headers linux-docs broadcom-wl
 
  genfstab -U /mnt >> /mnt/etc/fstab
 
