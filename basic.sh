52d1d74f2c5603f3982e37f41499c3e8b32df643
Shanjiv A <65117343+shanjiv177@users.noreply.github.com>
2021-11-09 17:44:57 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -12,7 +12,7 @@ mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
 reflector -l 10 -a 6 --sort rate --save /etc/pacman.d/mirrorlist --verbose
 
-pacstrap -S linux linux-firmware nano vim intel-ucode linux-headers linux-docs broadcom-wl
+pacstrap /mnt linux linux-firmware nano vim intel-ucode linux-headers linux-docs broadcom-wl
 
  genfstab -U /mnt >> /mnt/etc/fstab
 
