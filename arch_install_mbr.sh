a9058c130106c6ed7ee210aef358e6390ae5c5d9
Shanjiv A <65117343+shanjiv177@users.noreply.github.com>
2021-11-13 05:39:11 +0530

    Update arch_install_mbr.sh

--- arch_install_mbr.sh
+++ arch_install_mbr.sh
@@ -8,7 +8,7 @@ echo "myarchpc" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 myarchpc.localdomain    arch" >> /etc/hosts
-echo root:shanjiv1707 | chpasswd
+echo root:password | chpasswd
 
 pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant linux-headers base-devel mtools dosfstools reflector git curl wget xdg-user-dirs xdg-utils ntfs-3g bluez bluez-utils os-prober
 
@@ -20,9 +20,12 @@ systemctl enable bluetooth
 
 useradd -mG wheel shanjiv
 
-echo shanjiv:shanjiv1707 | chpasswd
+echo shanjiv:password | chpasswd
 
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
 echo "Type exit and umount -a"
+echo "Change Your root password with passwd"
+echo "Change your use password with passwd shanjiv"
+echo "Default User name is shanjiv"
 
